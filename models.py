7b47854891fce93f58e400e0d1f840c10b1256e6
DianaValieva <didarphin@yandex.ru>
2023-12-05 21:36:34 +0300

    Hw8

---
+++ models.py
@@ -0,0 +1,98 @@
+class Product:
+    """
+    Класс продукта
+    """
+    name: str
+    price: float
+    description: str
+    quantity: int
+
+    def __init__(self, name, price, description, quantity):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.quantity = quantity
+
+    def check_quantity(self, quantity) -> bool:
+        """
+         Верните True если количество продукта больше или равно запрашиваемому
+            и False в обратном случае
+        """
+        if self.quantity >= quantity:
+            return True
+        else:
+            return False
+
+    def buy(self, quantity):
+        """
+         реализуйте метод покупки
+            Проверьте количество продукта используя метод check_quantity
+            Если продуктов не хватает, то выбросите исключение ValueError
+        """
+        if self.check_quantity(quantity):
+            self.quantity -= quantity
+        else:
+            raise ValueError
+
+    def __hash__(self):
+        return hash(self.name + self.description)
+
+
+class Cart:
+    """
+    Класс корзины. В нем хранятся продукты, которые пользователь хочет купить.
+    """
+
+    # Словарь продуктов и их количество в корзине
+    products: dict[Product, int]
+
+    def __init__(self):
+        # По-умолчанию корзина пустая
+        self.products = {}
+
+    def add_product(self, product: Product, buy_count=1):
+        """
+        Метод добавления продукта в корзину.
+        Если продукт уже есть в корзине, то увеличиваем количество
+        """
+        if product in self.products:
+            self.products[product] += buy_count
+        else:
+            self.products[product] = buy_count
+
+    def remove_product(self, product: Product, remove_count=None):
+        """
+        Метод удаления продукта из корзины.
+        Если remove_count не передан, то удаляется вся позиция
+        Если remove_count больше, чем количество продуктов в позиции, то удаляется вся позиция
+        """
+        if remove_count is None or remove_count > self.products[product]:
+            del self.products[product]
+        else:
+            self.products[product] -= remove_count
+
+    def clear(self):
+        self.products = {}
+
+    def get_total_price(self) -> float:
+        total_price = 0
+        for item in list(self.products.items()):
+            price = item[0].price * item[1]
+            total_price += price
+        return total_price
+
+    def buy(self):
+        """
+        Метод покупки.
+        Учтите, что товаров может не хватать на складе.
+        В этом случае нужно выбросить исключение ValueError
+        """
+        items = list(self.products.items())
+        for item in items:
+            if item[0].quantity < item[1]:
+                raise ValueError
+        for item in items:
+            product = item[0]
+            quantity = item[1]
+            product.buy(quantity)
+        self.clear()
---
+++ test_shop.py
@@ -0,0 +1,102 @@
+"""
+Протестируйте классы из модуля homework/models.py
+"""
+import pytest
+
+from models import Product, Cart
+
+
+@pytest.fixture
+def product():
+    return Product("book", 100, "This is a book", 1000)
+
+
+@pytest.fixture()
+def empty_cart():
+    return Cart()
+
+
+@pytest.fixture()
+def cart_with_product(product):
+    cart = Cart()
+    cart.add_product(product, 5)
+    return cart, product
+
+
+class TestProducts:
+    """
+    Тестовый класс - это способ группировки ваших тестов по какой-то тематике
+    Например, текущий класс группирует тесты на класс Product
+    """
+
+    def test_product_check_quantity(self, product):
+        assert not product.check_quantity(1500), "Некорректный расчет когда запрашиваемое значение больше"
+        assert product.check_quantity(100), "Некорректный запрос когда запрашиваемое значение меньше"
+        assert product.check_quantity(product.quantity), "Некорректный запрос когда запрашиваемое " \
+                                                         "значение равно количеству"
+
+    def test_product_buy(self, product):
+        #  напишите проверки на метод buy
+        product.buy(100)
+        assert product.quantity == 900
+
+    def test_product_buy_more_than_available(self, product):
+        #  напишите проверки на метод buy,
+        #  которые ожидают ошибку ValueError при попытке купить больше, чем есть в наличии
+        with pytest.raises(ValueError):
+            product.buy(1500)
+
+
+class TestCart:
+    """
+    Напишите тесты на методы класса Cart
+        На каждый метод у вас должен получиться отдельный тест
+        На некоторые методы у вас может быть несколько тестов.
+        Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
+    """
+
+    def test_add_new_product(self, empty_cart, product):
+        empty_cart.add_product(product=product)
+        assert empty_cart.products[product] == 1
+
+    def test_add_some_products(self, cart_with_product):
+        cart, product = cart_with_product
+        cart.add_product(product, 5)
+        assert cart.products[product] == 10
+
+    def test_remove_1_product(self, cart_with_product):
+        cart, product = cart_with_product
+        cart.remove_product(product, 1)
+        assert cart.products[product] == 4
+
+    def test_remove_all_products(self, cart_with_product):
+        cart, product = cart_with_product
+        cart.remove_product(product)
+        assert product not in cart.products
+
+    def test_remove_many_products(self, cart_with_product):
+        cart, product = cart_with_product
+        cart.remove_product(product, 10)
+        assert product not in cart.products
+
+    def test_clear(self, cart_with_product):
+        cart, product = cart_with_product
+        cart.clear()
+        assert len(cart.products) == 0
+
+    def test_get_total_price(self, cart_with_product):
+        cart, product = cart_with_product
+        price = cart.get_total_price()
+        assert price == 500
+
+    def test_buy(self, cart_with_product):
+        cart, product = cart_with_product
+        cart.buy()
+        assert len(cart.products) == 0
+        assert product.quantity == 995
+
+    def test_buy_more_than_available(self, cart_with_product):
+        cart, product = cart_with_product
+        cart.add_product(product, 1000)
+        with pytest.raises(ValueError):
+            cart.buy()
